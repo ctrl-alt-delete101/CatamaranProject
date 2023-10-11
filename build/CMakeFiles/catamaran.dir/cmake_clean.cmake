426be9dd4497c4db3a7d9492ca8032c754c318f7
Arjun Mudda <amudda@umd.edu>
2023-10-11 19:22:49 -0400

    changed file structure and added files

---
+++ build/CMakeFiles/catamaran.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/catamaran.dir/src/catamaran.cpp.o"
+  "CMakeFiles/catamaran.dir/src/thrusters.cpp.o"
+  "catamaran"
+  "catamaran.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/catamaran.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
